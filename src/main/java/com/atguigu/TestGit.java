d234da88cc1cae89000c840d21d1de400f612e8a
JHLau_glb <xxxxxxJHLau_glb>
2021-09-03 20:26:37 +0800

    idea项目第一次提交

---
+++ src/main/java/com/atguigu/TestGit.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author JHLau
+ * @create 2021-09-03 20:25
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println(1111111);
+    }
+}
