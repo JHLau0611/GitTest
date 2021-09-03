02a9ff399a63451d3c353255cda1896c2a062508
JHLau_glb <xxxxxxJHLau_glb>
2021-09-03 20:30:41 +0800

    hotfix分支第一次修改代码

--- src/main/java/com/atguigu/TestGit.java
+++ src/main/java/com/atguigu/TestGit.java
@@ -8,5 +8,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println(1111111);
         System.out.println("master...");
+        System.out.println("hotfix...");
     }
 }
